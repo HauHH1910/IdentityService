73888c2af9dd4056534ba75601b191e4d35f517d
Hoang Hai Hau <hhhau1910@gmail.com>
2024-08-04 16:40:12 +0700

    create new API in AuthenticationController

--- src/main/java/com/hauhh/controller/AuthenticationController.java
+++ src/main/java/com/hauhh/controller/AuthenticationController.java
@@ -1,6 +1,13 @@
 package com.hauhh.controller;
 
+import com.hauhh.common.ResponseData;
+import com.hauhh.dto.request.AuthenticationRequest;
+import com.hauhh.dto.response.AuthenticateResponse;
+import com.hauhh.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
+    private final AuthenticationService authenticationService;
 
+    @PostMapping("/login")
+    public ResponseData<AuthenticateResponse> login(@RequestBody AuthenticationRequest request) {
+        boolean authenticated = authenticationService.authenticate(request);
+        return ResponseData.<AuthenticateResponse>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .data(AuthenticateResponse.builder()
+                        .authenticated(authenticated)
+                        .build())
+                .build();
+    }
 
 }
