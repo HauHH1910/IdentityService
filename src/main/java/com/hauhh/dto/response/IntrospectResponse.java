ee88f2457b04ab7ab52011e24d25e8c54a5fbf58
Hoang Hai Hau <hhhau1910@gmail.com>
2024-08-04 20:36:31 +0700

    feat: create new dto IntrospectRequest and IntrospectResponse

---
+++ src/main/java/com/hauhh/dto/response/IntrospectResponse.java
@@ -0,0 +1,15 @@
+package com.hauhh.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class IntrospectResponse {
+
+    private boolean valid;
+
+}
