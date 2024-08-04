11bd49bcfd6d2ae65c62551923a8661ff32f552a
Hoang Hai Hau <hhhau1910@gmail.com>
2024-08-04 20:35:29 +0700

    feat: create new method in AuthenticationService and AuthenticationServiceImpl

--- src/main/java/com/hauhh/service/AuthenticationService.java
+++ src/main/java/com/hauhh/service/AuthenticationService.java
@@ -1,8 +1,16 @@
 package com.hauhh.service;
 
 import com.hauhh.dto.request.AuthenticationRequest;
+import com.hauhh.dto.request.IntrospectRequest;
+import com.hauhh.dto.response.AuthenticationResponse;
+import com.hauhh.dto.response.IntrospectResponse;
+import com.nimbusds.jose.JOSEException;
+
+import java.text.ParseException;
 
 public interface AuthenticationService {
 
-    boolean authenticate(AuthenticationRequest authenticationRequest);
+    AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest);
+
+    IntrospectResponse introspect(IntrospectRequest request) throws JOSEException, ParseException;
 }
--- src/main/java/com/hauhh/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/hauhh/service/impl/AuthenticationServiceImpl.java
@@ -1,25 +1,102 @@
 package com.hauhh.service.impl;
 
 import com.hauhh.dto.request.AuthenticationRequest;
+import com.hauhh.dto.request.IntrospectRequest;
+import com.hauhh.dto.response.AuthenticationResponse;
+import com.hauhh.dto.response.IntrospectResponse;
 import com.hauhh.enums.ErrorCode;
 import com.hauhh.exception.AppException;
 import com.hauhh.repository.UserRepository;
 import com.hauhh.service.AuthenticationService;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.NonFinal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
     private final UserRepository userRepository;
 
+    @NonFinal
+    @Value("${com.hauhh.key}")
+    private String key;
+
     @Override
-    public boolean authenticate(AuthenticationRequest authenticationRequest) {
-        var user = userRepository.findByUsername(authenticationRequest.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXIST));
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-        return passwordEncoder.matches(authenticationRequest.getPassword(), user.getPassword());
+
+        var user = userRepository.findByUsername(authenticationRequest.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXIST));
+
+        boolean authenticated = passwordEncoder.matches(authenticationRequest.getPassword(), user.getPassword());
+
+        if (!authenticated) {
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
+
+        var token = generateToken(authenticationRequest.getUsername());
+
+        return AuthenticationResponse.builder()
+                .authenticated(true)
+                .token(token)
+                .build();
+    }
+
+    //Verify token
+    @Override
+    public IntrospectResponse introspect(IntrospectRequest request) throws JOSEException, ParseException {
+        var token = request.getToken();
+
+        JWSVerifier verifier = new MACVerifier(key.getBytes());
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date expirationTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        var verifyToken = signedJWT.verify(verifier);
+
+        return IntrospectResponse.builder()
+                .valid(verifyToken && expirationTime.after(new Date()))
+                .build();
+    }
+
+    private String generateToken(String username) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+        //Hết hạn sau 1 giờ
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("com.hauhh")
+                .issueTime(new Date())
+                .expirationTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                ))
+                .claim("customClaim", "Custom")
+                .build();
+        Payload payload = new Payload(claimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+        try {
+            jwsObject.sign(new MACSigner(key.getBytes()));
+        } catch (JOSEException e) {
+            log.error("Error at [AuthenticationServiceImpl] - method [generateToken] {} ", e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return jwsObject.serialize();
     }
 }
