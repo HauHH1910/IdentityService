e053f6d3fba3e74fd5b14635117c723de790bf36
Hoang Hai Hau <hhhau1910@gmail.com>
2024-08-04 20:36:08 +0700

    chore: add new enum in ErrorCode.java

--- src/main/java/com/hauhh/enums/ErrorCode.java
+++ src/main/java/com/hauhh/enums/ErrorCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ErrorCode {
     USER_EXIST(1001, "User already exists"),
     USER_NOT_EXIST(1002, "User does not exist"),
+    UNAUTHENTICATED(1003, "Unauthenticated"),
     ;
     private final int code;
     private final String message;
