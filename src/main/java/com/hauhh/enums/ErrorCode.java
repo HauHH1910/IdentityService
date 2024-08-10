feacb5cc967fc81c82f08a9794336fd23ebb6012
HaiHau <hhhau1910@gmail.com>
2024-08-10 21:26:18 +0700

    Add new enum in ErrorCode

--- src/main/java/com/hauhh/enums/ErrorCode.java
+++ src/main/java/com/hauhh/enums/ErrorCode.java
@@ -2,15 +2,21 @@ package com.hauhh.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_EXIST(1001, "User already exists"),
-    USER_NOT_EXIST(1002, "User does not exist"),
-    UNAUTHENTICATED(1003, "Unauthenticated"),
+    USER_EXIST(1001, "User already exists", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXIST(1002, "User does not exist", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1003, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNCATEGORIZED(1004, "Unauthorized", HttpStatus.INTERNAL_SERVER_ERROR),
+    ACCESS_DENIED(1005, "Access denied", HttpStatus.FORBIDDEN),
     ;
+
     private final int code;
     private final String message;
+    private final HttpStatusCode statusCode;
 }
