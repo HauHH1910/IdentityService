324b9b5d194c97332d797ebaa89ddf999feb90a7
HaiHau <hhhau1910@gmail.com>
2024-08-10 21:27:29 +0700

    implement status code and handle Access deny in GlobalHandlerException

--- src/main/java/com/hauhh/exception/GlobalHandlerException.java
+++ src/main/java/com/hauhh/exception/GlobalHandlerException.java
@@ -4,6 +4,7 @@ import com.hauhh.common.ResponseData;
 import com.hauhh.common.ResponseError;
 import com.hauhh.enums.ErrorCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,10 +14,20 @@ public class GlobalHandlerException {
     @ExceptionHandler(AppException.class)
     public ResponseEntity<ResponseError> handleAppException(AppException appException) {
         ErrorCode errorCode = appException.getErrorCode();
-        return ResponseEntity.badRequest().body(ResponseError.builder()
-                .code(errorCode.getCode())
-                .message(errorCode.getMessage())
-                .build());
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(ResponseError.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ResponseError> handleAccessDeniedException(AccessDeniedException accessDeniedException) {
+        return ResponseEntity.status(ErrorCode.ACCESS_DENIED.getStatusCode())
+                .body(ResponseError.builder()
+                        .code(ErrorCode.ACCESS_DENIED.getCode())
+                        .message(ErrorCode.ACCESS_DENIED.getMessage())
+                        .build());
     }
 
 }
