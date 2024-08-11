27bbd2dbb63242926ccc48312c11554ee041dddb
HaiHau <hhhau1910@gmail.com>
2024-08-11 22:43:42 +0700

    validation with annotation exception

--- src/main/java/com/hauhh/exception/GlobalHandlerException.java
+++ src/main/java/com/hauhh/exception/GlobalHandlerException.java
@@ -2,6 +2,7 @@ package com.hauhh.exception;
 
 import com.hauhh.common.ResponseError;
 import com.hauhh.enums.ErrorCode;
+import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -11,17 +12,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.MethodNotAllowedException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalHandlerException {
 
+    private static final String MIN_ATTRIBUTE = "min";
+
     @ExceptionHandler(value = AppException.class)
     public ResponseEntity<ResponseError> handleAppException(AppException appException) {
         ErrorCode errorCode = appException.getErrorCode();
-        log.info("ErrorCode Code: {}", errorCode.getCode());
-        log.info("ErrorCode Message: {}", errorCode.getMessage());
+        log.info("[handleAppException] Code: {}", errorCode.getCode());
+        log.info("[handleAppException] Message: {}", errorCode.getMessage());
         return ResponseEntity.status(errorCode.getStatusCode())
                 .body(ResponseError.builder()
                         .code(errorCode.getCode())
@@ -48,18 +53,38 @@ public class GlobalHandlerException {
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseError> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ResponseError> handleValidation(MethodArgumentNotValidException exception) {
         String enumKey = Objects.requireNonNull(exception.getFieldError()).getDefaultMessage();
-        ErrorCode errorCode = ErrorCode.valueOf(enumKey);
-        log.info("ErrorCode Code: {}", errorCode.getCode());
-        log.info("ErrorCode Message: {}", errorCode.getMessage());
+        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, Object> attributes = null;
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation = exception.getBindingResult().getAllErrors().get(0).unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+
+            log.info(attributes.toString());
+
+        }catch (IllegalArgumentException e) {
+            log.error("[handleValidation] Error: {}", e.getMessage());
+        }
+
+        log.info("[handleValidation] Code: {}", errorCode.getCode());
+        log.info("[handleValidation] Message: {}", errorCode.getMessage());
 
         return ResponseEntity.status(errorCode.getStatusCode())
                 .body(ResponseError.builder()
                         .code(errorCode.getCode())
-                        .message(errorCode.getMessage())
+                        .message(Objects.nonNull(attributes) ?
+                            mapAttribute(errorCode.getMessage(), attributes) : errorCode.getMessage())
                         .build());
     }
 
+    private String mapAttribute(String message, Map<String, Object> attributes){
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
+    }
 
 }
