a2d5b5464a2f89540be565dc26b7715eb4fc6f27
HaiHau <hhhau1910@gmail.com>
2024-08-11 16:47:22 +0700

    change code and message

--- src/main/java/com/hauhh/controller/AuthenticationController.java
+++ src/main/java/com/hauhh/controller/AuthenticationController.java
@@ -5,11 +5,9 @@ import com.hauhh.dto.request.AuthenticationRequest;
 import com.hauhh.dto.request.IntrospectRequest;
 import com.hauhh.dto.response.AuthenticationResponse;
 import com.hauhh.dto.response.IntrospectResponse;
-import com.hauhh.enums.ResponseCode;
 import com.hauhh.service.AuthenticationService;
 import com.nimbusds.jose.JOSEException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +25,7 @@ public class AuthenticationController {
     @PostMapping("/token")
     public ResponseData<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
         return ResponseData.<AuthenticationResponse>builder()
-                .code(HttpStatus.OK.value())
-                .message(HttpStatus.OK.getReasonPhrase())
+                .message("Login successful")
                 .result(authenticationService.authenticate(request))
                 .build();
     }
@@ -36,8 +33,7 @@ public class AuthenticationController {
     @PostMapping("/introspect")
     public ResponseData<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
         return ResponseData.<IntrospectResponse>builder()
-                .code(ResponseCode.SUCCESS.getCode())
-                .message(ResponseCode.SUCCESS.getMessage())
+                .message("Introspect successful")
                 .result(authenticationService.introspect(request))
                 .build();
     }
