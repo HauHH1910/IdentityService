068e58a619da367e0edc331d7a20d8256a8bf307
HaiHau <hhhau1910@gmail.com>
2024-08-11 16:51:13 +0700

    create new dto for Role

---
+++ src/main/java/com/hauhh/dto/response/RoleResponse.java
@@ -0,0 +1,20 @@
+package com.hauhh.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoleResponse {
+
+    private String name;
+    private String description;
+    private Set<PermissionResponse> permissions;
+
+}
