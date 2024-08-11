fd091430f308cf84325c742b0953b1f5079c1c3d
HaiHau <hhhau1910@gmail.com>
2024-08-11 20:50:48 +0700

    Setter in AppException

--- src/main/java/com/hauhh/exception/AppException.java
+++ src/main/java/com/hauhh/exception/AppException.java
@@ -2,7 +2,9 @@ package com.hauhh.exception;
 
 import com.hauhh.enums.ErrorCode;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class AppException extends RuntimeException{
 
