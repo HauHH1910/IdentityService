880c134e27811d0d6f111e62ed3bf6d3f87cd51f
HaiHau <hhhau1910@gmail.com>
2024-08-11 19:09:56 +0700

    add new authen annotation

--- src/main/java/com/hauhh/service/impl/RoleServiceImpl.java
+++ src/main/java/com/hauhh/service/impl/RoleServiceImpl.java
@@ -11,6 +11,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -27,6 +28,7 @@ public class RoleServiceImpl implements RoleService {
     PermissionRepository permissionRepository;
 
     @Override
+    @PreAuthorize("hasRole('ADMIN')")
     public RoleResponse createRole(RoleRequest request) {
         var role = roleMapper.toRole(request);
 
