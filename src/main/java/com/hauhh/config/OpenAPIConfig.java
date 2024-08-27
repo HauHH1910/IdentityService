5ef64c8efa2e6daa351ca62c8e8032437d56b2d8
HaiHau <hhhau1910@gmail.com>
2024-08-27 22:59:37 +0700

    OpenAPI Config

---
+++ src/main/java/com/hauhh/config/OpenAPIConfig.java
@@ -0,0 +1,56 @@
+package com.hauhh.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openAPI(@Value("${open.api.title}") String titleDocument,
+                            @Value("${open.api.version}") String versionDocument,
+                            @Value("${open.api.description}") String descriptionDocument,
+                            @Value("${open.api.server.url}") String serverDocument,
+                            @Value("${open.api.server.description}") String serverDescriptionDocument) {
+        return new OpenAPI().info(new Info()
+                .title(titleDocument)
+                .version(versionDocument)
+                .description(descriptionDocument)
+                .license(new License()
+                        .name("API License")
+                        .url("https://github.com/HauHH1910/Spring-Boot"))
+        ).servers(List.of(new Server()
+                .url(serverDocument)
+                .description(serverDescriptionDocument)
+        )).components(
+                new Components()
+                        .addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+        ).security(List.of(new SecurityRequirement().addList("bearerAuth")));
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenAPI(){
+        return GroupedOpenApi.builder()
+                .group("API-Service")
+                .packagesToScan("com.hauhh.controller")
+                .build();
+    }
+
+}
