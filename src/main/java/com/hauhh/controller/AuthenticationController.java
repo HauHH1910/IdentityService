b1a76351f1886c37daf36a75d23711aff8509cd8
HaiHau <hhhau1910@gmail.com>
2024-08-27 22:59:26 +0700

    Config Swagger in AuthenticationController

--- src/main/java/com/hauhh/controller/AuthenticationController.java
+++ src/main/java/com/hauhh/controller/AuthenticationController.java
@@ -11,6 +11,8 @@ import com.hauhh.dto.response.IntrospectResponse;
 import com.hauhh.dto.response.RefreshResponse;
 import com.hauhh.service.AuthenticationService;
 import com.nimbusds.jose.JOSEException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
 
 @RestController
+@Tag(name = "Authentication Controller")
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
+
+    @Operation(
+            summary = "Authentication",
+            description = "Login to the account using JWT"
+    )
     @PostMapping("/token")
     public ResponseData<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
         return ResponseData.<AuthenticationResponse>builder()
@@ -34,6 +42,10 @@ public class AuthenticationController {
                 .build();
     }
 
+    @Operation(
+            summary = "Introspect JWT token ",
+            description = "Check if the JWT token is valid or not"
+    )
     @PostMapping("/introspect")
     public ResponseData<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
         return ResponseData.<IntrospectResponse>builder()
@@ -42,6 +54,10 @@ public class AuthenticationController {
                 .build();
     }
 
+    @Operation(
+            summary = "Logout",
+            description = "Logout from the account"
+    )
     @PostMapping("/logout")
     public ResponseData<Void> logout(@RequestBody LogoutRequest request) throws ParseException, JOSEException {
         authenticationService.logout(request);
@@ -50,6 +66,10 @@ public class AuthenticationController {
                 .build();
     }
 
+    @Operation(
+            summary = "Refresh JWT token",
+            description = "Refresh JWT token"
+    )
     @PostMapping("/refresh")
     public ResponseData<RefreshResponse> refresh(@RequestBody RefreshRequest request) throws ParseException, JOSEException {
         return ResponseData.<RefreshResponse>builder()
