ca6b438524cc2a36a4c889feaa7e024e4996dae4
HaiHau <hhhau1910@gmail.com>
2025-01-18 20:51:11 +0700

    apply i18n in ErrorConstant

--- src/main/java/com/hauhh/models/enums/ErrorConstant.java
+++ src/main/java/com/hauhh/models/enums/ErrorConstant.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatusCode;
 @Getter
 @AllArgsConstructor
 public enum ErrorConstant {
-    USER_EXIST(1001, "User already exists", HttpStatus.BAD_REQUEST),
+    USER_EXIST(1001, "user.exist", HttpStatus.BAD_REQUEST),
     USER_NOT_EXIST(1002, "User does not exist", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1003, "Unauthenticated", HttpStatus.UNAUTHORIZED),
     UNCATEGORIZED(1004, "Unauthorized", HttpStatus.INTERNAL_SERVER_ERROR),
